f0a8855d53ce8a94c0567a2243e5ea02027fdc8a
tomfraczek <tomaszfr90@gmail.com>
2019-07-29 14:07:56 +0100

    First commit

---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.css';
+
+function Homepage() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    );
+}
+
+export default Homepage;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './navi.css';
+
+
+
+function Nav() {
+
+    return (
+        <div className='navi'>
+            <h1>Punk Api</h1>
+        </div>
+    );
+}
+
+export default Nav;
